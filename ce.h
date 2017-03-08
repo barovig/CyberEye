0d6544443998a1a56482e7920dcd7f78470dc062
barovig <semargl1349@gmail.com>
2017-03-08 17:23:59 +0000

    Added global header ce.h

---
+++ ce.h
@@ -0,0 +1,13 @@
+#ifndef CE_H
+#define CE_H
+
+#include "cybereye/coremanager.h"
+#include "cybereye/engine.h"
+#include "cybereye/tracker.h"
+#include "cybereye/imgobj.h"
+#include "cybereye/collector.h"
+#include "cybereye/model.h"
+#include "cybereye/dispatcher.h"
+#include "cybereye/bkgsegmentationengine.h"
+
+#endif // CE_H
