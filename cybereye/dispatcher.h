a1b9d567d57abcc8fda8e6978078ee5d1b41333f
barovig <semargl1349@gmail.com>
2017-04-11 12:09:07 +0100

    Added Boost to CMakeLists.txt

--- cybereye/dispatcher.h
+++ cybereye/dispatcher.h
@@ -1,6 +1,10 @@
 #ifndef CE_DISPATCHER_H
 #define CE_DISPATCHER_H
 #include "imgobj.h"
+#include "opencv2/core.hpp"
+#include <boost/serialization/binary_object.hpp>
+#include <boost/serialization/split_free.hpp>
+#include <boost/serialization/vector.hpp>
 
 namespace ce {
 
@@ -13,4 +17,40 @@ public:
 
 } // namespace cybeye
 
+// Mat serialisation
+BOOST_SERIALIZATION_SPLIT_FREE(::cv::Mat)
+namespace boost{
+namespace serialization{
+    template<class Archive>
+    void save(Archive & ar, const ::cv::Mat& m, const unsigned int v)
+    {
+      size_t elem_sz = m.elemSize();
+      size_t type = m.type();
+ 
+      ar & m.cols;
+      ar & m.rows;
+      ar & elem_sz;
+      ar & type;
+ 
+      const size_t data_sz = m.cols * m.rows * elem_sz;
+      ar & boost::serialization::make_array(m.ptr(), data_sz);
+    }
+	template<class Archive>
+    void load(Archive & ar, ::cv::Mat& m, const unsigned int v)
+    {
+      int cols, rows;
+      size_t elem_sz, type;
+ 
+      ar & cols;
+      ar & rows;
+      ar & elem_sz;
+      ar & type;
+ 
+      m.create(rows, cols, type);
+ 
+      size_t data_size = m.cols * m.rows * elem_sz;
+      ar & boost::serialization::make_array(m.ptr(), data_size);	
+	}
+}
+}
 #endif // CE_DISPATCHER_H
